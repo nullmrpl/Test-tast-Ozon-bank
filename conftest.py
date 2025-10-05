347778e8656583142f4818361c94c9bc7e4b1d13
nullmrpl <marinaplohotnikova33@gmail.com>
2025-10-05 21:14:45 +0300

    add tests

--- conftest.py
+++ conftest.py
@@ -1,6 +1,13 @@
 import pytest
 import logging
 
+from pathlib import Path
+
 @pytest.fixture(scope="session", autouse=True)
 def logger_setting():
     logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+@pytest.fixture(scope="session", autouse=False)
+def file_path():
+    test_file_dir = Path(__file__).parent
+    return Path(test_file_dir, "all_data_about_superhero.json")
--- test_func_get_tallest_superhero.py
+++ test_func_get_tallest_superhero.py
@@ -2,11 +2,104 @@ import os
 import pytest
 from func_get_tallest_superhero import get_tallest_superhero
 import logging
+import json
+import requests
 
 logger = logging.getLogger(__name__)
 
+@pytest.fixture(scope='module', autouse=True)
+def setip_teardown(file_path):
+    logger.info("Setup")
+    session = requests.Session()
+    logger.info("Get all data for check")
+    with open(file_path, 'w+') as file:
+        id = 1
+        while True:
+            res = session.get(f"https://www.superheroapi.com/api/{os.getenv('ACCESS_TOKEN', None)}/{id}")
+            res = res.json()
+            if res["response"] == "error" and res["error"] == "invalid id":
+                break 
+            json.dump(res, file)
+            file.write('\n')
+            id += 1
+    
+    yield
 
-def test_get_tallest_superhero():
-    logger.debug("Hi, I'm hero")
-    res = get_tallest_superhero('male', True)
-    assert res
+    logger.info("Teardown")
+    if file_path.exists():
+        os.remove(file_path)
+
+
+
+@pytest.mark.parametrize("gender,is_working", [
+    ("Male", True),
+    ("Male", False),
+    ("Female", True),
+    ("Female", False),
+    ("mALe", True),
+    ("-", True),
+    ("-", False),
+    ("Fimale", True),
+])
+def test_correct_params(file_path, gender, is_working):
+    res_return_func = get_tallest_superhero(gender, is_working)
+
+    max_height = None
+    tallest_superhero_id_list = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            item = json.loads(line)
+            if item["appearance"]["gender"].lower() != gender.lower():
+                continue
+            if item["work"]["occupation"] and item["work"]["occupation"] != "-":
+                if "cm" in item["appearance"]["height"][1]:
+                    cur_height = float(item["appearance"]["height"][1].removesuffix(" cm"))
+                elif "meters" in item["appearance"]["height"][1]:
+                    cur_height = float(item["appearance"]["height"][1].removesuffix(" meters")) * 100
+                else:
+                    continue
+                if not max_height or cur_height > max_height:
+                    tallest_superhero_id_list.clear()
+                    tallest_superhero_id_list.append(item)
+                    max_height = cur_height
+                elif max_height and cur_height == max_height:
+                    tallest_superhero_id_list.append(item)
+
+    assert res_return_func == tallest_superhero_id_list, "Результат функции не верный"
+
+
+@pytest.mark.parametrize("gender,is_working", [
+    (100, True)
+])
+def test_uncorrect_type_gender(file_path, gender, is_working):
+    with pytest.raises(TypeError) as e:
+        res_return_func = get_tallest_superhero(gender, is_working)
+    assert "Ошибка: переменная gender должна быть типа str" in str(e.value), (
+        "Функция не обработала неверный параметр gender")
+
+
+@pytest.mark.parametrize("gender,is_working", [
+    ("", True)
+])
+def test_empty_gender(file_path, gender, is_working):
+    with pytest.raises(ValueError) as e:
+        res_return_func = get_tallest_superhero(gender, is_working)
+    assert "Ошибка: переменная gender пустая" in str(e.value), (
+        "Функция не обработала пустой параметр gender")
+
+
+@pytest.mark.parametrize("gender,is_working", [
+    ("Female", "False")
+])
+def test_uncorrect_type_is_working(file_path, gender, is_working):
+    with pytest.raises(TypeError) as e:
+        res_return_func = get_tallest_superhero(gender, is_working)
+    assert "Ошибка: переменная is_working должна быть типа bool" in str(e.value), (
+        "Функция не обработала неверный параметр gender")
+
+
+def test_uncorrect_token(monkeypatch):
+    monkeypatch.setenv("ACCESS_TOKEN", "1t1t1t1t1t1tt1t1")
+    with pytest.raises(Exception) as e:
+        res_return_func = get_tallest_superhero("Male", True)
+    assert "Ошибка: access denied" in str(e.value), "Функция не обработала неверный токен"
